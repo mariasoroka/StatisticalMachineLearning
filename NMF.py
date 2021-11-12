cc69feeccb97bec71b41d4cdfaa4132a1ef74b20
Maria <maria_soroka@outlook.com>
2021-11-12 08:40:51 +0100

    first templates

---
+++ NMF.py
@@ -0,0 +1,29 @@
+
+class Spectrogram:
+    def __init__(self):
+        pass
+    def compute_matrix(self):
+        pass
+    def visualize(self):
+        pass
+
+class Recording:
+    def __init__(self, filename):
+        pass
+    def compute_spectrogram(self):
+        pass
+
+class NMF:
+    # stores the matrix for factorization
+    def __init__(self):
+        pass
+    def factorize_IS(self):
+        pass
+    def factorize_EM_IS(self):
+        pass
+    def factorize_EUC(self):
+        pass
+    def factorize_KL(self):
+        pass
+    def wiener_reconstruction(self):
+        pass
